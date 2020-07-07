c2a2e19a6a44ead07784d79df1d3e1269d2e46c4
kufei88 <117672825@qq.com>
2020-07-07 15:25:26 +0800

    异常机制上传

--- src/main/java/com/xcxgf/cainiao/services/RepairsService.java
+++ src/main/java/com/xcxgf/cainiao/services/RepairsService.java
@@ -1,6 +1,8 @@
 package com.xcxgf.cainiao.services;
 
 import com.xcxgf.cainiao.POJO.Repairs;
+import com.xcxgf.cainiao.common.enums.BaseServiceException;
+import com.xcxgf.cainiao.common.enums.ServiceErrCode;
 import com.xcxgf.cainiao.mapper.RepairsMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +16,9 @@ public class RepairsService {
     RepairsMapper repairsMapper;
 
     public int addRepairs(Repairs repairs){
-
+        if (repairs==null){
+            throw new BaseServiceException("参数不能为空！", ServiceErrCode.NOTFOUND_RESULT_ERR);
+        }
         SimpleDateFormat sdf=new SimpleDateFormat("yyyy/MM/dd hh:mm");
         repairs.setCreateTime(sdf.format(new Date()));
         return repairsMapper.addRepair(repairs);
