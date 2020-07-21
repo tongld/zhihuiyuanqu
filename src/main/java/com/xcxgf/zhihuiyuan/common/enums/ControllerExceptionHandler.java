a094ac822c7527e6a90c0faa0431153ffb553f26
gddgong <1403515328@qq.com>
2020-07-21 10:39:41 +0800

    gddgong

--- src/main/java/com/xcxgf/zhihuiyuan/common/enums/ControllerExceptionHandler.java
+++ src/main/java/com/xcxgf/zhihuiyuan/common/enums/ControllerExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.xcxgf.zhihuiyuan.common.enums;
 
-import org.apache.commons.collections.map.HashedMap;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
@@ -15,7 +15,7 @@ public class ControllerExceptionHandler {
     @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String,Object> handleUserNotExitException(BaseServiceException ex){
-        Map<String,Object> map  = new HashedMap();
+        Map<String,Object> map  = new HashMap<>();
         map.put("code",ex.getCode());
         map.put("message",ex.getMessage());
         return map;
